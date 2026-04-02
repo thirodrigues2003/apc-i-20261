03e73554b43e04e5f98203802e220f5e623d0768
thirodrigues2003 <thirodrigues2003@gmail.com>
2026-04-01 21:44:52 -0300

    pratica03 pendente

---
+++ praticas/pratica03/declara_constantes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    #define PI 3.14159265
+    const double Euler = 2.71828182;
+
+    printf("PI: \n", PI);
+    return 0;
+}
---
+++ praticas/pratica03/declara_variaveis.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int idade;
+    char sexo;
+    float altura;
+    double peso;
+
+    idade = 30;
+    sexo = 'M';
+    altura = 1.72f;
+    peso = 64.310;
+
+    printf("%s %i\n %s %c\n %s %.2f\n %s %.3lf\n", "Idade: ", idade, "Sexo: ", sexo, "Altura: ", altura, "Peso: ", peso);
+
+    return 0;
+}
